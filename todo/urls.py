a7e2005263ced5b75133682f126f178a907c234a
masashiSumiishi <t15519ms@gmail.com>
2021-12-17 16:51:18 +0900

    詳細を表示

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.TodoList.as_view(), name="list"),
+    path("detail/<int:pk>", views.TodoDetail.as_view(), name="detail")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from todo.models import Todo
 
 class TodoList(ListView):
     model = Todo
     context_object_name = "tasks"
+
+class TodoDetail(DetailView):
+    model = Todo
+    context_object_name = "task"
