f102dde2c79fe05f052ad70252bbd84e1b8a0e7d
masashiSumiishi <t15519ms@gmail.com>
2021-12-17 21:05:27 +0900

    新規作成機能

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.TodoList.as_view(), name="list"),
-    path("detail/<int:pk>", views.TodoDetail.as_view(), name="detail")
+    path("detail/<int:pk>", views.TodoDetail.as_view(), name="detail"),
+    path("create/", views.TodoCreate.as_view(), name="create")
 ]
